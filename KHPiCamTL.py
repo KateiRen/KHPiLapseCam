6364461e1d10280ea63ec1fe4014c67493de3ba4
Kateiren <pi@raspberry1.(none)>
2015-09-05 13:02:48 +0200

    Timing funktioniert jetzt

--- KHPiCamTL.py
+++ KHPiCamTL.py
@@ -12,8 +12,7 @@
 
 # Optionen für Framerate und End-Dauer des Videos und Dauer der Aufnahme als Basis für Delay und Max_Shots
 
-# Ueberschrift auf alle 20 Zeilen aendern
-# beim ersten Mal wird es doppel angezeigt
+# Camera Settings um dritte Spalte für "1/200 Sekunde" erweitern
 
 
 from datetime import datetime 
@@ -37,22 +36,22 @@ MIN_INTER_SHOT_DELAY_SECONDS = timedelta(seconds=30)
 MIN_BRIGHTNESS = 20000 
 MAX_BRIGHTNESS = 30000 
 
-dopostprocessing = True
-dodeflicker = False
+dovideoencoding = True
+dodeflicker = True
 verbosemode = False
 useraspistill = True
 displaycapturedimages = False
 
-# Startzeit abspeichern und am Ende mit der Endzeit anzeigen
 
-fps = 1
-videolength = 1 # Minutes
-capturelength = 1 # Minutes
+fps = 12
+videolength = 0.5 # Minutes
+capturelength = 30 # Minutes
 totalshots = videolength * 60 * fps
-intershotdelay = timedelta(seconds=capturelength * 60 / totalshots) # intershotdelay unit = timedelta in seconds
+#intershotdelay = timedelta(seconds=capturelength * 60 / totalshots) # intershotdelay unit = timedelta in seconds
+intershotdelay = capturelength * 60 / totalshots # [Sekunden]
 
 def main():
-    
+    teststart=float(time.time()) # Startzeit abspeichern und am Ende mit der Endzeit anzeigen
     idy = Identify(subprocess) # das ist ImageMagick
     if verbosemode==True:
         logging.basicConfig(level=logging.DEBUG)
@@ -61,7 +60,8 @@ def main():
     print "#######################"
     print "\nAufnahmedauer: %d Minuten" % capturelength
     print "Anzahl Aufnahmen: %d" % totalshots
-	
+    print "Pause zwischen den Fotos: %d Sekunden" % intershotdelay
+
     current_config = int((len(CONFIGS)-1)/2) # in der Mitte der Einstellungen starten
     width = 1296
     height = 730
@@ -69,18 +69,19 @@ def main():
     prev_acquired = None
     last_acquired = None
     last_started = None
+    comment="Start"
     
 # Unterordner für die Bilder der Serie mit datetime.now() erstellen
     timestr = time.strftime("%Y%m%d-%H%M%S")
     os.mkdir(timestr, 0755 )
     
     try:
+        #das erste Mal außerhalb der Schleife, später schon nach dem Sleep damit Änderungen der Belichtung mit gestoppt werden
+        starttime = float(time.time())
         while shot < totalshots:
-            last_started = datetime.now()
+            #last_started = datetime.now()
             config = CONFIGS[current_config]
-            #print "Auslösung: %d Belichtungszeit :%.2f sek ISO: %d" % (shot, float(config[0])/1000000, config[1])
             filename = timestr + '/image%05d.jpg' % shot
-            #print "filename %s" % filename
             if useraspistill == False:
                 with picamera.PiCamera() as camera:
                     camera.exif_tags['IFD0.Artist'] = 'Karsten Hartlieb'
@@ -102,31 +103,45 @@ def main():
                 os.system("raspistill " + optionstring)
                 
 
-            prev_acquired = last_acquired
+            #prev_acquired = last_acquired
             brightness = float(idy.mean_brightness(filename))
-            last_acquired = datetime.now()
+            #last_acquired = datetime.now()
             
             if shot%20 == 0:
-				# alle paar Zeilen die Überschrift widerholen
-				print "\nBild Nummer | Belichtungszeit | ISO | Helligkeit"
-				print "------------------------------------------------"
-            print "      %05d |      %.2f sek   | %d | %s " % (shot, float(config[0])/1000000, config[1], brightness)
-            #print "-> Datei: %s Helligkeit: %s" % (filename, brightness)
+                # alle paar Zeilen die Überschrift widerholen
+                print "\nBild Nr.| Belichtungszeit | ISO | Helligkeit | Kommentar"
+                print "------------------------------------------------------------"
+            print "  %05d |      %.2f sek   | %d | %s | %s" % (shot, float(config[0])/1000000, config[1], brightness, comment)
 
             if brightness < MIN_BRIGHTNESS and current_config < len(CONFIGS) - 1:
                 current_config = current_config + 1
+                comment = "Belichtungskorrektur"
                 if verbosemode == True:
                     print "Mittlere Helligkeit=%0.2f, erhöhe Belichtungszeit/ISO" %brightness
             elif brightness > MAX_BRIGHTNESS and current_config > 0:
                 current_config = current_config - 1
+                comment = "Belichtungskorrektur"
                 if verbosemode == True:
                     print "Mittlere Helligkeit=%0.2f, erhöhe Belichtungszeit/ISO" %brightness
             else:
-                if last_started and last_acquired and last_acquired - last_started < intershotdelay:
-                    if verbosemode==True:
-					    print "Sleeping for %s" % str(intershotdelay - (last_acquired - last_started))
+                comment=""
+                endtime = float(time.time()) #datetime.now()
+                #print "Dauer der Auslösung: %f" % (endtime-starttime)
+                #if ((endtime - starttime) > timedelta(seconds=int(intershotdelay))):
+                #if (int((endtime - starttime).strftime("%S")) > intershotdelay):
+                if ((int(endtime) - int(starttime)) < intershotdelay):
+                    #print "bis hier gehts noch"
+                    #time.sleep((intershotdelay - (endtime - starttime)).seconds)
+                    time.sleep(intershotdelay - (endtime - starttime))
+                else:
+                    comment="Zeitüberschreitung"
+                #if last_started and last_acquired and last_acquired - last_started < intershotdelay:
+                    #if verbosemode==True:
+                        #print "Sleeping for %s" % str(intershotdelay - (last_acquired - last_started))
                     
-                    time.sleep((intershotdelay - (last_acquired - last_started)).seconds)
+                    #time.sleep((intershotdelay - (last_acquired - last_started)).seconds)
+                    
+            starttime = float(time.time())
             shot = shot + 1
             
             # Wenn gewünscht, jedes Bild nach dem Schießen auf dem TFT anzeigen aber Konsolen-Ausgabe unterdrücken
@@ -136,27 +151,35 @@ def main():
 
     except Exception,e:
         print str(e)
-	
+
+    testende = float(time.time())
+
+    print "Geplante Dauer der Aufnahmephase: %d Stunden, %d Minuten" % (capturelength/60, capturelength - int(capturelength/60)*60)
+    print "Reale Dauer der Serie: %d Stunden, %d Minuten" % ((testende - teststart)/3600, (testende - teststart)/60- int((testende - teststart)/3600)*60)
+
     if displaycapturedimages == True:
         # Ausgabe wieder auf Terminal zurückbiegen
         # klappt das?
         os.system("con2bmap 1 1")
-    print "\nEnde der Aufnahmephase\n"
-	
-	#weil das Skript mit SU läuft, gehören auch alle Files root, das korrigieren wir mal schnell noch
-    os.system("chown -R pi " + timestr)
-     
-    # Wenn gewünscht, nach dem Schießen der Einzelbilder gleich als Video zusammensetzen        
-    if dopostprocessing == True:
-        if dodeflicker == True:
-            print "Starte De-Flicker Post-Processing"
-            commandstring = "for a in "+timestr+"/*.jpg; do echo $a;mogrify -auto-gamma $a;done"
-            print commandstring
-            os.system(commandstring)
-        print "Starte Erstellung des Videos"
-        commandstring = "sudo avconv -y -r 24 -i %s/image\%05d.jpg -an -vcodec libx264 -q:v 1 time-lapse.mp4" %  timestr
+    print "\nEnde der Aufnahmephase"
+
+    if dodeflicker == True:
+        print "\nStarte De-Flicker Post-Processing\n"
+        commandstring = "for a in "+timestr+"/*.jpg; do echo $a;mogrify -auto-gamma $a;done"
         print commandstring
         os.system(commandstring)
+
+    # Wenn gewünscht, nach dem Schießen der Einzelbilder gleich als Video zusammensetzen        
+    if dovideoencoding == True:
+        print "\nStarte Erstellung des Videos\n"
+        #commandstring = "sudo avconv -y -r 24 -i %s/image\%05d.jpg -an -vcodec libx264 -q:v 1 time-lapse.mp4" %  timestr
+        #commandstring = "sudo avconv -y -r 24 -i " + timestr +"/image\%05d.jpg -an -vcodec libx264 -q:v 1 time-lapse.mp4"
+        commandstring = "sudo avconv -y -r 24 -i " + timestr +"/image%05d.jpg -an -vcodec libx264 -q:v 1 " + timestr + "/time-lapse.mp4"
+        print commandstring
+        os.system(commandstring)
+
+    #weil das Skript mit SU läuft, gehören auch alle Files root, das korrigieren wir mal schnell noch
+    os.system("chown -R pi " + timestr)
 
  
 
